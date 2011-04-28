5716ae4511fd095b6768fc93fcf63cc8494054b3
harpywar@gmail.com <harpywar@gmail.com@5d7abbe1-27f8-cb71-bef1-01d8ca34fe51>
2011-04-28 13:02:46 +0000

    dutch language
    git-svn-id: https://pvpgn-magic-builder.googlecode.com/svn/trunk@6 5d7abbe1-27f8-cb71-bef1-01d8ca34fe51

--- module/i18n.inc.bat
+++ module/i18n.inc.bat
@@ -15,6 +15,7 @@ goto ENG
 :switch_lang
 :: switch languages
 if [%LANGUAGE%]==[RUS] goto RUS
+if [%LANGUAGE%]==[NLD] goto NLD
 ::if [%LANGUAGE%]==[POL] goto POL
 
 :echo_phrase
@@ -94,3 +95,40 @@ goto :eof
 	set PHRASE_3_10=Информацию об изменениях можно посмотреть в файле "version-history.txt"
 	
 	goto echo_phrase
+	
+:: translation by MusicDemon
+:NLD
+	set PHRASE_1_1= Visual Studio is niet geinstaleerd
+	set PHRASE_1_2= Visual Studio C++ omgeving was niet gevonden!
+	set PHRASE_1_3=Download/update de laatste PvPGN broncode van de SVN (in de "source" directory)?
+	set PHRASE_1_4=  PvPGN zal updaten van de SVN
+	set PHRASE_1_5=  PvPGN zal niet worden geupdate
+	set PHRASE_1_6=Selecteer PvPGN omgeving:
+	set PHRASE_1_7=   1) Console (defaut)
+	set PHRASE_1_8=   2) GUI
+	set PHRASE_1_9=Kies een nummer
+	set PHRASE_1_10=  Zet PvPGN omgeving als Console
+	set PHRASE_1_11=  Set PvPGN interface as GUI (Graphical User Interface) Zet PvPGN omgeving als GUI ()
+	set PHRASE_1_12= Selecteer een database type:
+	set PHRASE_1_13=   1) Normaal (Text) (Standaard)
+	set PHRASE_1_14=   2) MySQL
+	set PHRASE_1_15=  PvPGN zal gebouwen worden zonder database ondersteuning
+	set PHRASE_1_16=CMake configuratie mislukt
+   
+	set PHRASE_2_1=Beschikbare %2 versies (u kunt uw eigen in modules\includes\%2):
+	set PHRASE_2_2=   Kies een nummer
+	set PHRASE_2_3=   Verkeerde keuze... Probeer het opnieuw
+	set PHRASE_2_4=   PvPGN word gebouwd met %2 ondersteuning op v%3
+   
+	set PHRASE_3_1=Checken voor updates...
+	set PHRASE_3_2=U hebt versie "v%2"
+	set PHRASE_3_3=Nieuwe versie is "v%2"
+	set PHRASE_3_4= U hebt de laatste versie van PvPGN Magic Builder
+	set PHRASE_3_5=Externe versie van PvPGN Magic Builder is niet gelijk aan met de jouwe. Wilt u automatisch bijgewerkt naar de nieuwe versi
+	set PHRASE_3_6= Update was geannuleerd door gebruiker
+	set PHRASE_3_7=Starten van update...
+	set PHRASE_3_8= Downloaden van bestand "%2"...
+	set PHRASE_3_9=Update voltooid
+	set PHRASE_3_10=Controleer het bestand "version-history.txt" voor meer informatie over wijzigingen
+   
+	goto echo_phrase   
