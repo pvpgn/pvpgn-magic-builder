c4fc53056eb41f7f5d97e5f31c8a1be9c8bc7b6e
HarpyWar <harpywar@gmail.com>
2015-01-31 13:51:48 +0300

    2.07 release

--- build_pvpgn.bat
+++ build_pvpgn.bat
@@ -10,7 +10,7 @@ color 1f
 echo.
 echo *:*:*:*:*:*:*:*:*-  P v P G N  M a g i c  B u i l d e r  -*:*:*:*:*:*:*:*:*:*:*
 echo *                                                                             *
-echo *   Copyright 2011-2014, HarpyWar (harpywar@gmail.com)                        *
+echo *   Copyright 2011-2015, HarpyWar (harpywar@gmail.com)                        *
 echo *   http://harpywar.com                                                       *
 echo *                                                                             *
 echo *:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*:*
